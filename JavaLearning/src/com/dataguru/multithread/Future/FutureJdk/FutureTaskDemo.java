1075cc526122bafc6d3e0a801429de25aa6aecf5
zhihao.guo <zhihaoguo@foxmail.com>
2016-03-22 10:27:39 +0800

    future demo

--- JavaLearning/src/com/dataguru/multithread/Future/FutureJdk/FutureTaskDemo.java
+++ JavaLearning/src/com/dataguru/multithread/Future/FutureJdk/FutureTaskDemo.java
@@ -6,7 +6,7 @@ import java.util.concurrent.*;
  * Created by zhihaoguo on 16/3/21.
  * zhihao.guo@dianping.com
  */
-public class Application {
+public class FutureTaskDemo {
     public static void main(String[] args) throws InterruptedException, ExecutionException {
         FutureTask<String> futureTask = new FutureTask<String>(new RealData("name"));
         ExecutorService executor = Executors.newFixedThreadPool(1);
@@ -16,6 +16,7 @@ public class Application {
         //代表别的业务
         Thread.sleep(1000);
         System.out.println("数据="+futureTask.get());
+        executor.shutdown();
 
 
     }
